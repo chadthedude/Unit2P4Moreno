62b4d6c17c9dde118c114df6a34d7499466f208c
PetetheCATLOVER <724626@student.dusd.net>
2025-03-26 11:40:59 -0700

    finished main part
    finished main part of the game, still working on bonus features

--- Unit2BasicGameplayMorenoP4/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2BasicGameplayMorenoP4/Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,11 +6,12 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
-    
+    private float sideBound = 30;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,7 +23,15 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        else if (transform.position.x > sideBound)
+        {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
+    
 }
+
--- Unit2BasicGameplayMorenoP4/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGameplayMorenoP4/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public float zRange = 5;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+
         //Keep the player in bounds
         if (transform.position.x < -xRange)
         {
@@ -28,6 +31,18 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, -transform.position.y, -zRange);
+        }
+        if(transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
