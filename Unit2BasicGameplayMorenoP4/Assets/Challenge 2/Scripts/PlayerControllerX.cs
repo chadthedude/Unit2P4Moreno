dc46401495c71b35eb4980be6cf6f5e72b5a3f94
PetetheCATLOVER <724626@student.dusd.net>
2025-04-10 11:42:56 -0700

    finiseds
    nneed to record challenge 2

---
+++ Unit2BasicGameplayMorenoP4/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    [SerializeField] float dogCooldown;
+    float coolTimer = Mathf.Infinity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        coolTimer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && coolTimer >=dogCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolTimer = 0;
+        }
+    }
+}
